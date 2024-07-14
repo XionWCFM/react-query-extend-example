c3f73213cb16dc9d8cc5c87a4feecc055771f925
devgiljong <calf-love@naver.com>
2024-07-14 17:12:44 +0900

    :construction: Obersver test

--- app/observer/page.tsx
+++ app/observer/page.tsx
@@ -7,64 +7,66 @@ type Observer<T> = (data: T) => void;
 class Observable<T> {
   private observers: Observer<T>[] = [];
 
-  addObserver(observer: Observer<T>): void {
+  subscribe(observer: Observer<T>): void {
     this.observers.push(observer);
   }
 
-  removeObserver(observer: Observer<T>): void {
+  unsubscribe(observer: Observer<T>): void {
     this.observers = this.observers.filter((obs) => obs !== observer);
   }
 
-  notifyObservers(data: T): void {
+  notify(data: T): void {
     this.observers.forEach((observer) => observer(data));
   }
 }
 
-const noop = () => {};
+class Counter extends Observable<number> {
+  count: number;
+  constructor() {
+    super();
+    this.count = 0;
+  }
+  increment() {
+    this.dispatch((prev) => prev + 1);
+  }
 
-const ObserverContainer = <T,>(
-  props: PropsWithChildren & { observable: Observable<T>; observers?: Observer<T>[]; observer?: Observer<T> },
-) => {
-  const { observable, observers, observer, children } = props;
-  const callback = usePreservedCallback(observer ?? noop);
-  useEffect(() => {
-    observers?.forEach((observer) => observable.addObserver(observer));
-    observable.addObserver(callback);
-    return () => {
-      observers?.forEach((observer) => observable.removeObserver(observer));
-      observable.removeObserver(callback);
-    };
-  }, [observable, observers, callback]);
+  decrement() {
+    this.dispatch((prev) => prev - 1);
+  }
 
-  return children;
-};
+  getSnapshot() {
+    return this.count;
+  }
 
-const hello = new Observable<number>();
-
-export default function Page() {
-  const [state, setState] = useState(0);
-
-  return (
-    <ObserverContainer
-      observable={hello}
-      observer={(data) => {
-        setState(data);
-      }}
-    >
-      <div className="">현재 데이터 {state}</div>
-      <Comp />
-    </ObserverContainer>
-  );
+  private dispatch(fn: (num: number) => number) {
+    this.count = fn(this.count);
+    this.notify(this.count);
+  }
 }
 
-const Comp = () => {
+const counter = new Counter();
+
+const handleClick = () => {
+  counter.increment()
+}
+const NotWorkingComponent = () => {
+  const [count, setCount] = useState(() => counter.getSnapshot());
+  useEffect(() => {
+    counter.subscribe(setCount);
+    return () => {
+      counter.unsubscribe(setCount);
+    };
+  }, []);
+
   return (
-    <button
-      onClick={() => {
-        hello.notifyObservers(15);
-      }}
-    >
-      클릭
-    </button>
+    <div>
+      <div>{counter.count}</div>
+      <div>{count}</div>
+      <button onClick={handleClick}>increment</button>
+    </div>
   );
 };
+
+export default function Page() {
+  return <NotWorkingComponent />;
+}
