f73cfafc18fa4a50918a400bc43b1370c61d9f2b
devgiljong <calf-love@naver.com>
2024-09-21 16:23:53 +0900

    :construction: Embla page remove

---
+++ src/loading-image.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { cn } from "@xionwcfm/xds";
+import { type ComponentProps, Fragment, type ReactNode, useState } from "react";
+import Image from "next/image";
+
+export const LoadingImage = (props: ComponentProps<typeof Image> & { fallback?: ReactNode }) => {
+  const { onLoad, fallback, className, ...rest } = props;
+  const [isLoaded, setLoaded] = useState(false);
+  const isRenderFallback = !isLoaded;
+  return (
+    <Fragment>
+      {isRenderFallback ? fallback : null}
+      <Image
+        className={cn(!isLoaded && " absolute invisible", className)}
+        onLoad={(e) => {
+          setLoaded(true);
+          onLoad?.(e);
+        }}
+        {...rest}
+      />
+    </Fragment>
+  );
+};
